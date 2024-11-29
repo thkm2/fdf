84c0c150db96eb8caa5b87fdc514a6f5d1ca2ef9
Kim <thkm.grd@gmail.com>
2024-11-29 18:12:26 +0100

    ...

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kgiraud <kgiraud@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 16:25:56 by kgiraud           #+#    #+#             */
-/*   Updated: 2024/11/26 16:43:50 by kgiraud          ###   ########.fr       */
+/*   Updated: 2024/11/29 18:11:36 by kgiraud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int	main(int ac, char **av)
 	t_fdf	*env;
 
 	if (ac != 2)
-		return (0);
+	{
+		ft_putendl_fd("Usage: ./fdf <map.fdf>", 2);
+		return (1);
+	}
 	env = ft_env_init(av[1]);
 	ft_parse_map(av[1], env->map);
 	ft_hook(env);
